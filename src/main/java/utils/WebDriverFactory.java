97672eaaab5c0ea2770574f4b800d71b34e73be7
poshyvailov <pelmel12@gmail.com>
2020-08-19 21:39:54 +0300

    New project

---
+++ src/main/java/utils/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverFactory {
+    private static WebDriver webDriver;
+
+    public static WebDriver getDriver() {
+        return webDriver;
+    }
+
+    public static void createInstance(String browserName) {
+
+        WebDriver driver = null;
+
+        if (browserName.toLowerCase().contains("firefox")) {
+            WebDriverManager.firefoxdriver().setup(); // Аналог - System.setProperty("webdriver.chrome.driver","D:\List_of_Jar\chromedriver.exe"); и руками не кладем фафлик в папку
+            driver = new FirefoxDriver();
+        } else if (browserName.toLowerCase().contains("internet")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        } else if (browserName.toLowerCase().contains("chrome")) {
+            // WebDriverManager.chromedriver().version("84.0.4147.30").setup();
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else {
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
+        webDriver = driver;
+    }
+
+}
